37ddcc395dedf694ce6d0749746be543f1bf5177
Abdullah Shams <shams3049@gmail.com>
2021-02-10 16:45:18 +0100

    Initial commit from React Studio 1.7.39

---
+++ src/StartScreen.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import './App.css';
+import Camera from './Camera';
+
+export default class StartScreen extends Component {
+
+  // Properties used by this component:
+  // appActions, deviceInfo
+
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+    };
+  }
+
+  componentDidMount() {
+  }
+
+  componentWillUnmount() {
+  }
+
+  componentDidUpdate() {
+  }
+
+  render() {
+    let layoutFlowStyle = {};
+    let baseStyle = {};
+    if (this.props.transitionId && this.props.transitionId.length > 0 && this.props.atTopOfScreenStack && this.props.transitionForward) {
+      baseStyle.animation = '0.25s ease-in-out '+this.props.transitionId;
+    }
+    if ( !this.props.atTopOfScreenStack) {
+      layoutFlowStyle.height = '100vh';
+      layoutFlowStyle.overflow = 'hidden';
+    }
+    
+    const style_elBackground = {
+      width: '100%',
+      height: '100%',
+     };
+    const style_elBackground_outer = {
+      backgroundColor: '#f6f6f6',
+     };
+    const style_elCamera = {
+      pointerEvents: 'auto',
+     };
+    
+    return (
+      <div className="AppScreen StartScreen" style={baseStyle}>
+        <div className="background">
+          <div className="containerMinHeight elBackground" style={style_elBackground_outer}>
+            <div className="appBg" style={style_elBackground} />
+          </div>
+        </div>
+        
+        <div className="layoutFlow" style={layoutFlowStyle}>
+          <div className="elCamera">
+            <div style={style_elCamera}>
+              <Camera ref={(el)=> this._elCamera = el} appActions={this.props.appActions} deviceInfo={this.props.deviceInfo} locStrings={this.props.locStrings} />
+            </div>
+          </div>
+        </div>
+        
+      </div>
+    )
+  }
+  
+}
